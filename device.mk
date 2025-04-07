a57c0ae3d00edfdaa32b043a746c7084bcafd25e
Michael Bestas <mkbestas@lineageos.org>
2025-04-07 23:07:34 +0300

    milanf: Set up dt2w through power HAL
    Change-Id: Ib5c815c2d534e2d266271fff87deac31a6f16b65
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device.mk
+++ device.mk
@@ -25,7 +25,6 @@ $(call inherit-product, device/motorola/sm6375-common/common.mk)
 PRODUCT_PACKAGES += \
     FrameworksResDevice \
     LineageApertureAppDevice \
-    LineageSystemUIDevice \
     SystemUIResDevice \
     WifiResDevice
 
@@ -67,9 +66,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
 # Sensors
-PRODUCT_PACKAGES += \
-    sensors.milanf
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
