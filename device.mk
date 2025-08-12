0305b9bce2a0d69b53fed8b3a7b20a9405308cb2
AnierinB <anierin@evolution-x.org>
2025-08-12 13:04:51 -0700

    rhodep: Rebrand to milanf
    Change-Id: I0206cf1bf86e6b30a69bfa4fb651ad14e324b621
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device.mk
+++ device.mk
@@ -4,7 +4,7 @@
 #
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_HEIGHT := 2460
 TARGET_SCREEN_WIDTH := 1080
 
 # Screen
@@ -77,7 +77,7 @@ $(foreach DEVICE_SKU, $(DEVICE_SKUS), \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    sensors.rhodep
+    sensors.milanf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
@@ -86,4 +86,4 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # Inherit from vendor blobs
-$(call inherit-product, vendor/motorola/rhodep/rhodep-vendor.mk)
+$(call inherit-product, vendor/motorola/milanf/milanf-vendor.mk)
