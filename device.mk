338297691e7230eb128eb7d579263f1ac676c82b
AnierinB <anierin@evolution-x.org>
2025-08-12 13:04:51 -0700

    rhodep: Rebrand to milanf
    Change-Id: I0206cf1bf86e6b30a69bfa4fb651ad14e324b621
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device.mk
+++ device.mk
@@ -77,7 +77,7 @@ $(foreach DEVICE_SKU, $(DEVICE_SKUS), \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    sensors.rhodep
+    sensors.milanf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
@@ -86,4 +86,4 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # Inherit from vendor blobs
-$(call inherit-product, vendor/motorola/rhodep/rhodep-vendor.mk)
+$(call inherit-product, vendor/motorola/milanf/milanf-vendor.mk)
