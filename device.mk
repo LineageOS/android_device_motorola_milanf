8965dab3e68b29b8dcdd473369cc40ef5a37542f
Bruno Martins <bgcngm@gmail.com>
2025-11-16 19:58:14 +0530

    milanf: Set Lineage Health control charging path
    This must now be set per device after migrating to soong_config_set.
    
    Change-Id: I5dd2801391b0e790bafa81bde81fe2d7b737fb30

--- device.mk
+++ device.mk
@@ -59,6 +59,9 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 $(call soong_config_set,livedisplay_sysfs,enable_ab,true)
 
+# Lineage Health
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/device/force_charging_disable)
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \
